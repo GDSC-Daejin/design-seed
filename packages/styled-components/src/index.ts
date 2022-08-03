aac2a4eed627dc69dae1d4778bfbde83054e2da6
HyeokE <jhjeong00@gmail.com>
2022-08-03 23:52:50 +0900

    chore: add useDarkmode in index.ts

--- packages/styled-components/src/index.ts
+++ packages/styled-components/src/index.ts
@@ -1,5 +1,6 @@
 export { GdsThemeProvider } from './GdsThemeProvider';
 export { DarkModeContext } from './DarkModeContext';
+export { useDarkMode } from './useDarkMode';
 export type {
   ColorScheme,
   isColorGroup,
