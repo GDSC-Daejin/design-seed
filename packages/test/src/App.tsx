4941d6cff1e38141290dbdff691922e219c4a3c2
HyeokE <jhjeong00@gmail.com>
2022-11-20 20:21:41 +0900

    feat: add themeHandler

---
+++ packages/test/src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+import { themeHandler, changeTheme } from '@gdsc-dju/gds-theme/themeHandler';
+
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  themeHandler();
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className={'glass-effect'}>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => changeTheme()}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/theme/themeHandler.ts
@@ -0,0 +1,39 @@
+export function themeHandler() {
+  const el = document.documentElement;
+  el.dataset.seed = '';
+  const savedTheme = localStorage.getItem('theme');
+  const prefersLight = window.matchMedia('(prefers-color-scheme: light)');
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+  function apply() {
+    el.dataset.seedScaleColor = prefersDark.matches ? 'dark' : 'light';
+    localStorage.setItem('theme', el.dataset.seedScaleColor);
+  }
+  if (savedTheme) {
+    el.dataset.seedScaleColor = savedTheme;
+  } else {
+    if (prefersLight.matches) {
+      if ('addEventListener' in prefersLight) {
+        prefersLight.addEventListener('change', apply);
+      }
+    } else if (prefersDark.matches) {
+      if ('addEventListener' in prefersDark) {
+        prefersDark.addEventListener('change', apply);
+      }
+    }
+  }
+  apply();
+}
+export function changeTheme(theme?: string) {
+  const el = document.documentElement;
+
+  const currentTheme = localStorage.getItem('theme');
+  if (theme) {
+    el.dataset.seedScaleColor = theme;
+    localStorage.setItem('theme', theme);
+  } else {
+    currentTheme === 'dark'
+      ? localStorage.setItem('theme', 'light')
+      : localStorage.setItem('theme', 'dark');
+    el.dataset.seedScaleColor = currentTheme === 'dark' ? 'light' : 'dark';
+  }
+}
