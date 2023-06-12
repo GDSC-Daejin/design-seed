ad6d347cfe696e99fd5aa4133ddeb11160a14acb
Jason-Jeong-Dev <jasonjeong.dev@gmail.com>
2023-06-12 20:23:33 +0900

    chore: fix color tokens

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,7 +1,7 @@
-export * as color from './color';
-export * as fontSize from './fontSize';
-export * as fontWeight from './fontWeight';
-export * as windowSize from './windowSize';
+export * as colors from './color';
+export * as fontSizes from './fontSize';
+export * as fontWeights from './fontWeight';
+export * as windowSizes from './windowSize';
 export { themeHandler, changeTheme } from './themeHandler';
 export { isColorToken, isFontSizeToken, isFontWeightToken } from './util';
 export { useTheme } from './hooks/useTheme';
