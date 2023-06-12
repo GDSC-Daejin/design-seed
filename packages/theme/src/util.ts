5dfd971466acddc39015ffdcce58ff629c95ca8d
Jason-Jeong-Dev <jasonjeong.dev@gmail.com>
2023-06-12 20:28:49 +0900

    chore: fix tokens

--- packages/theme/src/util.ts
+++ packages/theme/src/util.ts
@@ -1,14 +1,14 @@
-import { color, fontSize, fontWeight } from './index';
+import { colors, fontSizes, fontWeights } from './index';
 
 export const isColorToken = (token: string): boolean => {
-  const colorTokens = Object.keys(color);
+  const colorTokens = Object.keys(colors);
   return colorTokens.includes(token);
 };
 export const isFontSizeToken = (token: string): boolean => {
-  const fontSizeTokens = Object.keys(fontSize);
+  const fontSizeTokens = Object.keys(fontSizes);
   return fontSizeTokens.includes(token);
 };
 export const isFontWeightToken = (token: string): boolean => {
-  const fontWeightTokens = Object.keys(fontWeight);
+  const fontWeightTokens = Object.keys(fontWeights);
   return fontWeightTokens.includes(token);
 };
