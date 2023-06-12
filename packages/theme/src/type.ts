655732cdce3bdc0416b1c332f31be16c5f98c07f
Jason-Jeong-Dev <jasonjeong.dev@gmail.com>
2023-06-12 20:11:56 +0900

    feat: add token type

--- packages/theme/src/type.ts
+++ packages/theme/src/type.ts
@@ -1,1 +1,7 @@
+import { color, fontSize, fontWeight } from 'src';
+
 export type ThemeType = 'light-only' | 'dark-only' | 'auto';
+
+export type ColorToken = keyof typeof color;
+export type FontSizeToken = keyof typeof fontSize;
+export type FontWeightToken = keyof typeof fontWeight;
